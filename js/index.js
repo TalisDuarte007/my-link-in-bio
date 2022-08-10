b29b68aaf46928a6a6547a107ae0623fe9a5f4dc
TalisDuarte007 <tcd07@hotmail.com>
2022-08-10 16:53:56 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+window.addEventListener("DOMContentLoaded", () => {
+  VANTA.BIRDS({
+    el: "#vanta",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.0,
+    minWidth: 200.0,
+    scale: 1.0,
+    scaleMobile: 1.0,
+    color1: 0xff0f,
+    color2: 0xef00ff,
+    colorMode: "lerpGradient",
+    birdSize: 1.4,
+    speedLimit: 3.0,
+    separation: 10.0,
+    quantity: 4.0,
+    backgroundAlpha: 0.0,
+  })
+})
